2f24b619f9de8479dd62f4f388d9405799920d76
Felipe Domingues <felipe@domhubs.com.br>
2026-08-16 21:47:26 -0300

    LGPD: remove UPDATE restante com email do Grupo Forte

--- supabase/migrations/20260318190704_42ce8591-314b-48d8-ace6-5ede73d24c76.sql
+++ supabase/migrations/20260318190704_42ce8591-314b-48d8-ace6-5ede73d24c76.sql
@@ -1,3 +1,1 @@
-UPDATE user_company_access
-SET modules = array_replace(modules, 'financial_pagar', 'financial_receber')
-WHERE user_id = (SELECT id FROM auth.users WHERE email = 'fortecorporativa@gmail.com');
+-- [LGPD] UPDATE direcionado a usuario do Grupo Forte removido na limpeza do fork Braseg
